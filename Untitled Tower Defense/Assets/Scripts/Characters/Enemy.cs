5c3a7241a0de2bb764a07b3465cf8a72aba9fcbc
thuvu <thuvu354@gmail.com>
2025-05-19 01:42:22 -0500

    Created offensive attack for Defender prefab
    + Enemy prefab can take damage
    + Added health bar for Defender and Enemy prefabs

--- Untitled Tower Defense/Assets/Scripts/Characters/Enemy.cs
+++ Untitled Tower Defense/Assets/Scripts/Characters/Enemy.cs
@@ -3,41 +3,52 @@ using System.Collections;
 using Unity.Collections;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
 
-public enum DefenderType
+public enum EnemyType
 {
     None,
     Offensive,
     Defensive
 }
 
-public class Defender : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    public DefenderType defenderType;
+    public EnemyType defenderType;
     public GameObject projectilePrefab;
     public Transform target;
     public float shootForce = 5f;
     public float maxDistance = 20f;
+    public float maxHealth = 10;
+    public float currHealth = 10;
 
-    public bool isEnableAwake = false;
+    public Slider slider;
 
     public GameObject shooterNozzle;
     [Range(-20f, 20f)] public float xAngle = 0f;
     //[Range(-68f, 68f)] public float yAngle = 0f;
     [Range(-20f, 20f)] public float zAngle = 0f;
 
-    /* void Awake(){
-        gameObject.SetActive(isEnableAwake);
-    } */
 
     void OnEnable(){
         projectilePrefab.SetActive(false);
-        StartCoroutine(MultipleProjectiles(100));
+        currHealth = maxHealth;
+        //StartCoroutine(MultipleProjectiles(100));
     }
 
-    void Update(){
-        shooterNozzle.transform.localEulerAngles = new Vector3(xAngle, 0f, zAngle);
+    void OnTriggerEnter(Collider other){
+        Destroy(other.gameObject);
+        currHealth--;
+        slider.value = currHealth / maxHealth;
+
+        if(currHealth <= 0){
+            Destroy(gameObject);
+        }
     }
+
+    /* void Update(){
+        shooterNozzle.transform.localEulerAngles = new Vector3(xAngle, 0f, zAngle);
+    } */
 
     public IEnumerator MultipleProjectiles(int numProjectiles){
         int currNum = 0;
@@ -55,9 +66,11 @@ public class Defender : MonoBehaviour
         newProjectile.transform.SetParent(shooterNozzle.transform.parent);
         newProjectile.transform.rotation = shooterNozzle.transform.rotation;
         newProjectile.SetActive(true);
-        newProjectile.GetComponent<Rigidbody>().AddForce(directionWithoutSpread.normalized * shootForce, ForceMode.Impulse);
 
-        while(newProjectile != null && (newProjectile.transform.localPosition.x <= maxDistance && newProjectile.transform.localPosition.y <= maxDistance && newProjectile.transform.localPosition.x <= maxDistance)){
+        GameObject singleProjectile = newProjectile.transform.Find("Projectile").gameObject;
+        singleProjectile.GetComponent<Rigidbody>().AddForce(directionWithoutSpread.normalized * shootForce, ForceMode.Impulse);
+
+        while(singleProjectile != null && (singleProjectile.transform.localPosition.x <= maxDistance && singleProjectile.transform.localPosition.y <= maxDistance && singleProjectile.transform.localPosition.x <= maxDistance)){
             yield return null;
         }
 
